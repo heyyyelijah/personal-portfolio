03b08bb82ea47e331be23d193670959dab0b79da
Cj Elijah Garay <elijahmamuri@gmail.com>
2021-11-06 04:23:42 -0700

    added env and emails and procfile and requirements.txt

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, abort
-
+import requests
+from emails import sendEmail
+from decouple import config
 
 app = Flask(__name__)
+url = config('URL')
 
 
 @app.route('/')
 def home():
+    response = requests.request("GET", url)
+
+    sendEmail(response)
     return render_template("index.html", home_page=True)
 
 @app.route('/projects')
