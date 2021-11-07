6dead8a36d6e6550729794f30882c75f33a1752c
Cj Elijah Garay <elijahmamuri@gmail.com>
2021-11-06 17:42:08 -0700

    updated emails func and requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, abort
 import requests
-from emails import sendEmail
 from decouple import config
+import smtplib
 
 app = Flask(__name__)
 url = config('URL')
+
+MY_EMAIL = config('email')
+PASSWORD = config('password')
+
+
+def sendEmail(data):
+    data = data.json()
+    ip = data['ip']
+
+    if ip != config('my_ip'):
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="elijahmamuri@gmail.com",
+                msg=f"Subject: PORTFOLIO WAS VIEWED \n\n "
+                    f"ip address: {ip}\n"
+                    f"country: {data['country_name']}")
 
 
 @app.route('/')
