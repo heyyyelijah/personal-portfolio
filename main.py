0786f364efe36a54643941386b27613a59abf1bf
Cj Elijah Garay <elijahmamuri@gmail.com>
2021-11-08 17:31:04 -0800

    removed smtp

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ import smtplib
 app = Flask(__name__)
 url = config('URL')
 
-
-def sendEmail(data):
-    data = data.json()
-    ip = data['ip']
-
-    if ip != config('my_ip'):
-        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-            connection.starttls()
-            connection.login(user=MY_EMAIL, password=PASSWORD)
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs="elijahmamuri@gmail.com",
-                msg=f"Subject: PORTFOLIO WAS VIEWED \n\n "
-                    f"ip address: {ip}\n"
-                    f"country: {data['country_name']}")
+# MY_EMAIL = config('email')
+# PASSWORD = config('password')
+#
+# def sendEmail(data):
+#     data = data.json()
+#     ip = data['ip']
+#
+#     if ip == config('my_ip'):
+#         with smtplib.SMTP("smtp.yahoo.com", 587) as connection:
+#             connection.starttls()
+#             connection.login(user=MY_EMAIL, password=PASSWORD)
+#             connection.sendmail(
+#                 from_addr=MY_EMAIL,
+#                 to_addrs="elijahmamuri@gmail.com",
+#                 msg=f"Subject: PORTFOLIO WAS VIEWED \n\n "
+#                     f"ip address: {ip}\n"
+#                     f"country: {data['country_name']}")
 
 
 @app.route('/')
